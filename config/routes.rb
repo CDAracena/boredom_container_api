4e02ce76b832621d46ae8732ee999c4ad64d4683
CDAracena <cdaracena@gmail.com>
2019-03-30 21:20:52 -0400

    added users index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :api do 
   mount_devise_token_auth_for 'User', at: 'auth'
   end
+  get 'api/users', to: 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
